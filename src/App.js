629660afbb4a496e178ab0f8a5922bfcfeb071cd
Lucas Rohlin <lucas_rohlin@co-graph.com>
2024-10-09 08:51:49 +0900

    initial commit, created a flexible button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button"
+import { GoBell, GoFold, GoDatabase } from "react-icons/go"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="box">
+                <Button primary><GoBell />Primary</Button>
+                <Button secondary><GoFold />secondary</Button>
+                <Button success><GoDatabase />success</Button>
+                <Button warning>warning</Button>
+                <Button danger>danger</Button>
+                <Button>plain</Button>
+            </div>
+            <div className="box">
+                <Button primary rounded>Primary</Button>
+                <Button secondary rounded>secondary</Button>
+                <Button success rounded>success</Button>
+                <Button warning rounded>warning</Button>
+                <Button danger rounded>danger</Button>
+                <Button rounded>plain</Button>
+            </div>
+            <div className="box">
+                <Button primary rounded outline>Primary</Button>
+                <Button secondary rounded outline>secondary</Button>
+                <Button success rounded outline>success</Button>
+                <Button warning rounded outline>warning</Button>
+                <Button danger rounded outline>danger</Button>
+                <Button rounded outline>plain</Button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
